741037ec19333114d7cd3911eab13b90ef849351
S70126 <s70126@ocean.umt.edu.my>
2025-01-09 13:42:10 +0800

    Delete GroupProject directory

--- GroupProject/js/progress-bar.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('scroll', function () {
-    const progressBar = document.getElementById('progressBar');
-    const scrollTop = window.scrollY;
-    const documentHeight = document.documentElement.scrollHeight - window.innerHeight;
-    const scrollPercentage = (scrollTop / documentHeight) * 100;
-    progressBar.style.width = scrollPercentage + '%';
-});
